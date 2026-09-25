865fd69788f84797bdc799a5a7ecebf6c378f083
kthrnbeh <kthrnbeh@yahoo.com>
2026-09-25 14:09:42 -0600

    road map

--- Main.java
+++ Main.java
@@ -5,13 +5,32 @@ public class Main {
   //Play 3 rounds of rock paper scissors and declare winner
   //Get choice from player 1
   // Then player 2 or computer
-  
+   // If playing against computer:
+            // Generate computer's random choice
 
-   // ask if they want to play again
+        // If playing against another player:
+            // Get choice from player 2
+
+        // Check for a tie
+
+        // Check win conditions and decide who won
+
+        // Add 1 point to the winner's score
+
+        // Show scoreboard after each round
+
+    // After 3 rounds, show final scoreboard
+
+    // Show who won the whole game
+
+    // Ask if they want to play another 3 rounds
+
+
+   /
     public static void main(String[] args) {
         Random random = new Random();
         int number = random.nextInt(3);
         System.out.println(number);
     }
-    //Get random choice from 0 to 2
+    
 }
