91013ef3d1d6e2e4c69153e99628779d6fc9151c
kthrnbeh <kthrnbeh@yahoo.com>
2026-09-25 14:08:39 -0600

    road map

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
 import java.util.Random;
-
+//Rock paper scissors game
 public class Main {
+  //Ask player if they want a computer or another player
+  //Play 3 rounds of rock paper scissors and declare winner
+  //Get choice from player 1
+  // Then player 2 or computer
+  
 
+   // ask if they want to play again
     public static void main(String[] args) {
         Random random = new Random();
         int number = random.nextInt(3);
         System.out.println(number);
     }
+    //Get random choice from 0 to 2
 }
